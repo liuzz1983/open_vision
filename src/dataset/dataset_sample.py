c998dabf0eecf6f0f4f7a21133f945784a10068f
liuzz1983 <liuzz1983@gmail.com>
2017-04-09 21:02:20 +0800

    change code structure

---
+++ src/dataset/dataset_sample.py
@@ -0,0 +1,39 @@
+#coding=utf-8
+
+import sys
+import facenet
+import numpy as np
+
+def sample_people(dataset, people_per_batch, images_per_person):
+    nrof_images = people_per_batch * images_per_person
+  
+    # Sample classes from the dataset
+    nrof_classes = len(dataset)
+    class_indices = np.arange(nrof_classes)
+    np.random.shuffle(class_indices)
+    
+    i = 0
+    image_paths = []
+    num_per_class = []
+    sampled_class_indices = []
+    # Sample images from these classes until we have enough
+    while len(image_paths)<nrof_images:
+        print i,nrof_images,len(dataset)
+        class_index = class_indices[i]
+        nrof_images_in_class = len(dataset[class_index])
+        image_indices = np.arange(nrof_images_in_class)
+        np.random.shuffle(image_indices)
+        nrof_images_from_class = min(nrof_images_in_class, images_per_person, nrof_images-len(image_paths))
+        idx = image_indices[0:nrof_images_from_class]
+        image_paths_for_class = [dataset[class_index].image_paths[j] for j in idx]
+        sampled_class_indices += [class_index]*nrof_images_from_class
+        image_paths += image_paths_for_class
+        num_per_class.append(nrof_images_from_class)
+        i+=1
+  
+    return image_paths, num_per_class
+
+
+train_set = facenet.get_dataset(sys.argv[1])
+
+sample_people(train_set,45,40)
---
+++ src/tools/align_image.py
@@ -0,0 +1,140 @@
+"""Performs face alignment and stores face thumbnails in the output directory."""
+# MIT License
+# 
+# Copyright (c) 2016 David Sandberg
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+import os
+import argparse
+import random
+
+
+import cv2
+import numpy as np
+from scipy import misc
+import tensorflow as tf
+
+import facenet
+from align import detect_face
+
+import facenet
+import align.detect_face
+import align
+
+
+def read_image(image_path):
+    return misc.imread(image_path)
+
+def to_rgb(img):
+    w, h = img.shape
+    ret = np.empty((w, h, 3), dtype=np.uint8)
+    ret[:, :, 0] = ret[:, :, 1] = ret[:, :, 2] = img
+    return ret
+
+def draw_text(frame, text, x, y, color=(0,255,0), thickness=1, size=1):
+    if x is not None and y is not None:
+        cv2.putText(
+            frame, text, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, size, color, thickness)
+
+
+def align_image(image_path, output_path, gpu_memory_fraction=0.3,  margin=44, image_size=182):
+    with tf.Graph().as_default():
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+        with sess.as_default():
+            pnet, rnet, onet = align.detect_face.create_mtcnn(sess, None)
+
+
+    minsize = 20 # minimum size of face
+    threshold = [ 0.6, 0.7, 0.8 ]  # three steps's threshold
+    factor = 0.709 # scale factor
+
+    # Add a random key to the filename to allow alignment using multiple processes
+    random_key = np.random.randint(0, high=99999)
+
+    try:
+        img = misc.imread(image_path)
+    except (IOError, ValueError, IndexError) as e:
+        errorMessage = '{}: {}'.format(image_path, e)
+        print(errorMessage)
+    else:
+        if img.ndim<2:
+            print('Unable to align "%s"' % image_path)
+            text_file.write('%s\n' % (output_filename))
+            return 
+        if img.ndim == 2:
+            img = facenet.to_rgb(img)
+        img = img[:,:,0:3]
+
+        bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+
+        nrof_faces = bounding_boxes.shape[0]
+        print("========faces:", nrof_faces, bounding_boxes.shape)
+
+
+        if nrof_faces>0:
+            det = bounding_boxes[:,0:4]
+            img_size = np.asarray(img.shape)[0:2]
+            """if nrof_faces>1:
+                bounding_box_size = (det[:,2]-det[:,0])*(det[:,3]-det[:,1])
+                img_center = img_size / 2
+                offsets = np.vstack([ (det[:,0]+det[:,2])/2-img_center[1], (det[:,1]+det[:,3])/2-img_center[0] ])
+                offset_dist_squared = np.sum(np.power(offsets,2.0),0)
+                index = np.argmax(bounding_box_size-offset_dist_squared*2.0) # some extra weight on the centering
+
+                det = det[index,:]
+            """
+            dets = det
+            img2 = cv2.imread(image_path )
+        
+            for i in range(dets.shape[0]):
+
+                #print(dets[i, :])
+                det = np.squeeze(dets[i, :])
+                bb = np.zeros(4, dtype=np.int32)
+                bb[0] = np.maximum(det[0]-margin/2, 0)
+                bb[1] = np.maximum(det[1]-margin/2, 0)
+                bb[2] = np.minimum(det[2]+margin/2, img_size[1])
+                bb[3] = np.minimum(det[3]+margin/2, img_size[0])
+                cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
+                scaled = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
+
+                misc.imsave(os.path.join(output_path,str(i)+".jpeg"), scaled)
+                #text_file.write('%s %d %d %d %d\n' % (output_filename, bb[0], bb[1], bb[2], bb[3]))
+
+                bl = (bb[2], bb[3])
+                tr = (bb[0], bb[1])
+                color_cv = (255,0,0)
+                #print(bl, tr)
+                cv2.rectangle(img2, bl, tr, color=color_cv, thickness=2)
+                draw_text(img2, str(bb), bb[0], bb[1])
+
+            cv2.imwrite(os.path.join(output_path,"full.jpeg"), img2)
+        else:
+            print('Unable to align "%s"' % image_path)
+            text_file.write('%s\n' % (output_filename))
+            
+if __name__ == '__main__':
+    align_image(sys.argv[1], sys.argv[2])
